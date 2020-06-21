8de452c1b402b90639663ae1651997b960aadb28
chunza2542 <chunza2542@gmail.com>
2020-06-21 17:45:19 +0700

    Setup basic bot api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
 import * as functions from 'firebase-functions';
+import {WebClient} from "@slack/web-api";
+import {PubSub} from "@google-cloud/pubsub";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const bot = new WebClient(functions.config().slack.token);
+const pubsubClient = new PubSub();
+
+export const myBot = functions.https.onRequest(async (req, res) => {
+  const data = JSON.stringify(req.body);
+  const {challenge} = req.body;
+  const dataBuffer = Buffer.from(data);
+
+  await pubsubClient
+    .topic('slack-channel')
+    .publisher
+    .publish(dataBuffer);
+
+  res.status(200).send({challenge});
+  return;
+});
+
+export const slackChannel = functions.pubsub.topic('slack-channel')
+  .onPublish(async (message, ctx) => {
+    const {event} = message.json;
+
+    await bot.chat.postMessage({
+      channel: '#pay-me',
+      text: JSON.stringify(event),
+    })
+  });
+
